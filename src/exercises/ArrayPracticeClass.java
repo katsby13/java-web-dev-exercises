247d6692307c166f9a94009da0d0c430dba1623e
Katlyn McDonald <kkmcoding@gmail.com>
2022-07-21 00:04:21 -0400

    Added all parts of exercise for reference

--- src/exercises/ArrayPracticeClass.java
+++ src/exercises/ArrayPracticeClass.java
@@ -4,11 +4,24 @@ import java.util.Arrays;
 
 public class ArrayPracticeClass {
     public static void main(String[] args) {
+
+// Part 4:
+
         String sentence = "I would not, could not, in a box. I would not, could not with a fox. " +
                 "I will not eat them in a house. I will not eat them with a mouse.";
-        String[] sentenceArray = sentence.split("\\.");
 
+        String[] sentenceArray = sentence.split("\\.");
         System.out.println(Arrays.toString(sentenceArray));
+
+// Part 3:
+
+//        String sentence = "I would not, could not, in a box. I would not, could not with a fox. " +
+//                "I will not eat them in a house. I will not eat them with a mouse.";
+//
+//        String[] sentenceArray = sentence.split(" ");
+//        System.out.println(Arrays.toString(sentenceArray));
+
+// Part 2:
 
 //        int[] numArray = { 1, 1, 2, 3, 5, 8 };
 //
@@ -17,5 +30,15 @@ public class ArrayPracticeClass {
 //                System.out.println(i);
 //            }
 //        }
+
+// Part 1:
+
+//        int[] numArray = { 1, 1, 2, 3, 5, 8 };
+//
+//        for (int i : numArray) {
+//                System.out.println(i);
+//            }
+//        }
+
     }
 }
